71d36d26e56400188153f4d778cf5d49d079ff33
Hostile359 <Hostile359@github.com>
2019-07-24 12:28:38 +0700

    fixed task lombok

--- Accs/src/main/java/ru/eltex/User.java
+++ Accs/src/main/java/ru/eltex/User.java
@@ -9,47 +9,5 @@ abstract class User implements CSV{
     protected String phone;
     protected String email;
     
-    /*public Integer getId () {
 
-		return this.id;
-	}
-
-	public String getFio () {
-
-		return this.fio;
-	}
-
-	public String getPhone () {
-
-		return this.phone;
-	}
-
-	public String getEmail () {
-
-		return this.email;
-	}
-		
-	public void setId (String arg) {
-
-		this.id = Integer.valueOf(arg);
-	}
-	
-
-	public void setFio (String arg) {
-
-		this.fio = arg;
-	}
-
-	public void setPhone (String arg) {
-
-		this.phone = arg;
-	}
-
-	public void setEmail (String arg) {
-
-		this.email = arg;
-	}*/
-
-    //public String toCSV();
-    //public void fromCSV(String str);
 }
