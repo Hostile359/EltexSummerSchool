8797ce709b0568cfa46ba856d25d671a91abca47
Hostile359 <Hostile359@github.com>
2019-08-01 16:00:50 +0700

    task broadcast

---
+++ Broadcast/Server/src/main/java/ru/eltex/Main.java
@@ -0,0 +1,21 @@
+package ru.eltex;
+
+
+import java.io.*;
+import java.net.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(2973);
+        DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+        while (true) {
+            try {
+                socket.receive(packet);
+                System.out.println("Received from: " + packet.getAddress() + ":" + packet.getPort());
+            } catch (IOException error) {
+                error.getMessage();
+            }
+        }
+    }
+}
