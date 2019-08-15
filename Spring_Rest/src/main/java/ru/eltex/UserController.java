f52556d80f37e6eb8ad78a50342c5c5f98658343
Hostile359 <Hostile359@github.com>
2019-08-15 17:28:14 +0700

    task NodeJS server

--- Spring_Rest/src/main/java/ru/eltex/UserController.java
+++ Spring_Rest/src/main/java/ru/eltex/UserController.java
@@ -1,14 +1,13 @@
 package ru.eltex;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class UserController {
 
